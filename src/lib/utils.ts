d8df5b192357103d6d887ee911ce90bafa2f3576
ical10 <mochhusnir@gmail.com>
2025-11-19 16:09:03 +0800

    feat: install shadcn components

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,8 +1,10 @@
-import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+import type { Snippet } from 'svelte';
 
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
 }
 
 export type WithElementRef<T> = T & { ref?: any };
+export type WithoutChildren<T> = Omit<T, 'children'> & { children?: Snippet };
