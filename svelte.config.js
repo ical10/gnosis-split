1ab2012e3dfd3d8a685ab009e7ffbf2c17b68aea
ical10 <mochhusnir@gmail.com>
2025-11-21 00:02:38 +0800

    ci: add netlify files

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,18 @@ const config = {
   // Consult https://svelte.dev/docs/kit/integrations
   // for more information about preprocessors
   preprocess: vitePreprocess(),
-  kit: { adapter: adapter() }
+  kit: {
+    adapter: adapter({
+      // if true, will create a Netlify Edge Function rather
+      // than using standard Node-based functions
+      edge: false,
+
+      // if true, will split your app into multiple functions
+      // instead of creating a single one for the entire app.
+      // if `edge` is true, this option cannot be used
+      split: false
+    })
+  }
 };
 
 export default config;
