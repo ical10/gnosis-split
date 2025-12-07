f6062cde72f3fe8d701698d71addd7a47f2fb61c
ical10 <mochhusnir@gmail.com>
2025-12-07 16:43:10 +0800

    fix: configure vite-plugin-pwa to solve caching issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,36 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [tailwindcss(), sveltekit()]
+  plugins: [
+    tailwindcss(),
+    sveltekit(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: { enabled: true },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,svg,png}'],
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/.*\.(?:png|jpg|jpeg|svg|gif)$/,
+            handler: 'CacheFirst',
+            options: { cacheName: 'images' }
+          }
+        ]
+      },
+      manifest: {
+        name: 'Gnosis Split',
+        short_name: 'GnoSplit',
+        icons: [
+          {
+            src: '/gnosis-split-meta.jpg',
+            sizes: 'any',
+            type: 'image/jpeg'
+          }
+        ]
+      }
+    })
+  ]
 });
