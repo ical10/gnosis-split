b08ecbc244f76cee618c03960c729d085eea7715
ical10 <mochhusnir@gmail.com>
2025-12-07 20:49:34 +0800

    ci: configure all non-public env variables as private

--- svelte.config.js
+++ svelte.config.js
@@ -16,7 +16,10 @@ const config = {
       // instead of creating a single one for the entire app.
       // if `edge` is true, this option cannot be used
       split: false
-    })
+    }),
+    env: {
+      privatePrefix: ''
+    }
   }
 };
 
